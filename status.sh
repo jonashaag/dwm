238453a6bcf6d0141a041aff6a120fb8c7175216
Jonas Haag <jonas@lophus.org>
2012-11-02 02:52:54 +0100

    Refactor status, add pacman update count

--- status.sh
+++ status.sh
@@ -1,16 +1,41 @@
 #!/bin/sh
 
+
+cached() {
+  if [ ! -e /tmp/.dwm-$1-cache ] || [ 0 -eq $(($(date +'%s') % $2)) ]; then
+    # cache miss
+    eval "$3" > /tmp/.dwm-$1-cache
+  fi
+  cat /tmp/.dwm-$1-cache
+}
+
+
+updates() {
+  cached updates 86400 '
+    if [ -z "$(pgrep pacman)" ]; then
+      pkg -Sy > /dev/null
+      N=$(pkg -Qu 2>/dev/null | wc -l)
+      if [ "$N" -gt 0 ]; then
+        echo "$N updates"
+      fi
+    fi
+    '
+}
+
+
 fs() {
   echo $(statvfs /) / $(statvfs /jonas/)
 }
 
+
 bat() {
-  if [ ! -e /tmp/.dwm-bat-cache ] || [ 0 -eq $(($(date +'%s') % 10)) ]
-  then
-    acpi | cut -d : -f 2- | sed 's/^ //g' | tr '[:upper:]' '[:lower:]' | sed 's/\(dis\)\?charging, //' | sed 's/:[0-9]\+ / /' > /tmp/.dwm-bat-cache
-  fi
-  cat /tmp/.dwm-bat-cache
+  cached bat 10 "
+    acpi | cut -d : -f 2- \
+         | sed 's/^ //g' | tr '[:upper:]' '[:lower:]' \
+         | sed 's/\(dis\)\?charging, //' | sed 's/:[0-9]\+ / /'
+    "
 }
+
 
 mix() {
   volume=$(amixer get PCM | egrep -o '[0-9]+%' | uniq)
@@ -18,15 +43,16 @@ mix() {
   echo $volume $muted
 }
 
+
 clock() {
   date +'%a %b %d %H:%M:%S'
 }
 
+
 moc() {
   local tmp
   tmp=$(mocp --format '%t -- %a %ct/%tt' 2>/dev/null)
-  if [ 0 -eq $? ]
-  then
+  if [ 0 -eq $? ]; then
     echo "$tmp"
   fi
 }
@@ -37,14 +63,12 @@ main() {
   do
     STATUS=
 
-    for callback in fs bat moc mix clock
+    for callback in updates fs bat moc mix clock
     do
       local tmp
       tmp=$($callback)
-      if [ ! -z "$tmp" ]
-      then
-        if [ -z "$STATUS" ]
-        then
+      if [ ! -z "$tmp" ]; then
+        if [ -z "$STATUS" ]; then
           STATUS="$tmp"
         else
           STATUS="$STATUS | $tmp"
